e7bee19d6c8bfccc1e2e1d7eadfadf127dfcff9c
zzux-ppx <3785225584@qq.com>
2026-07-01 00:54:35 +0800

    fix: 修复部署问题

--- deploy/kafka/create-topics.sh
+++ deploy/kafka/create-topics.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 BROKER=${KAFKA_BROKER:-kafka:9092}
+KAFKA_TOPICS_BIN=${KAFKA_TOPICS_BIN:-/opt/kafka/bin/kafka-topics.sh}
 create_topic() {
   local topic=$1
   local partitions=${2:-6}
-  kafka-topics.sh --bootstrap-server "$BROKER" --create --if-not-exists --topic "$topic" --partitions "$partitions" --replication-factor 1
+  "$KAFKA_TOPICS_BIN" --bootstrap-server "$BROKER" --create --if-not-exists --topic "$topic" --partitions "$partitions" --replication-factor 1
 }
 # 一期主投递 Topic：群消息、撤回事件统一通过 eventType 区分，保证同群尽量落到同一分区。
 create_topic group-message-topic 12
